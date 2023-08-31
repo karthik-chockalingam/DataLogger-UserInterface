ebe290b47a620bd46172ff921dead6e1b3446366
karthik-chockalingam <karthik.chockalingam.09@gmail.com>
2023-08-31 19:49:03 +0200

    Added Backend Functionality

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -15,17 +14,26 @@ MainWindow::MainWindow(QWidget *parent)
     ui->comboBoxSampleRate->addItem("5 Sample/s");
     ui->comboBoxSampleRate->addItem("10 Sample/s");
 
-    // Disable channel and Log fields
+    // Disable all fields except TCP connection;
+    ui->pushButtonDisconnect->setDisabled(true);
     ui->groupBoxChannel->setDisabled(true);
+    ui->plainTextEditLogs->setDisabled(true);
+
+    //set Log message field read only
+    ui->plainTextEditLogs->setReadOnly(true);
 }
 
 MainWindow::~MainWindow()
 {
+    //stop the capture process -> Needed incase of abrupt disconnect during logging
+    MainWindow::on_pushButtonStop_clicked();
+
     if(clientsocket->isOpen())
         clientsocket->close();
     delete ui;
 }
 
+// Slot methods
 void MainWindow::on_pushButtonConnect_clicked()
 {
     QString Srv_IP_Addr_str = ui->lineEditIPAddress->text();
@@ -33,8 +41,9 @@ void MainWindow::on_pushButtonConnect_clicked()
     QStringList Srv_IP_Addr_strlist;
     unsigned int Srv_IP_Addr[4];
     unsigned int Srv_Port_Num;
+    int i = 0, j = 0, k = 0, l = 0;
 
-    // Check are there 4 parts of IP address
+    // IP address check - 4 address part using "." delimiter
     Srv_IP_Addr_strlist = Srv_IP_Addr_str.split(".");
     int num_of_parts = Srv_IP_Addr_strlist.size();
 
@@ -45,8 +54,8 @@ void MainWindow::on_pushButtonConnect_clicked()
     }
     else
     {
-        // Check is there any alphabet in the IP address
-        for(int i=0; i<4; i++)
+        // IP address check - Numbers only
+        for(i=0; i<4; i++)
         {
             if(Srv_IP_Addr_strlist[i].size() <= 0)
             {
@@ -58,7 +67,9 @@ void MainWindow::on_pushButtonConnect_clicked()
                 for(int j=0; j<Srv_IP_Addr_strlist[i].size(); j++)
                 {
                     bool is_digit = Srv_IP_Addr_strlist[i][j].isDigit();
+#if ENABLE_DEBUG
                     qInfo() << Srv_IP_Addr_strlist[i][j] << "is digit - " << is_digit;
+#endif
                     if(!(is_digit))
                     {
                         QMessageBox::warning(this, "Connection", "Invalid IP Address");
@@ -74,43 +85,52 @@ void MainWindow::on_pushButtonConnect_clicked()
         Srv_IP_Addr[3] = Srv_IP_Addr_strlist[3].toInt();
     }
 
-    // Check the IP addr range and not alpha
-    for(int i=0; i<4; i++)
+    // IP address check - Range(0 to 255)
+    for(k=0; k<4; k++)
     {
-        if(!((Srv_IP_Addr[i] <= 255) || (Srv_IP_Addr[i] >= 0)))
+        if(((Srv_IP_Addr[k] > 255) || (Srv_IP_Addr[k] < 0)))
         {
             QMessageBox::warning(this, "Connection", "Invalid IP Address");
             return;
         }
     }
 
-    // check is the port number alphabet
-
-    for(int i=0; i<Srv_Port_Num_str.size(); i++)
+    // Port Number check - Numbers only
+    if(!Srv_Port_Num_str.size())
     {
-        bool is_alpha = Srv_Port_Num_str[i].isLetter();
-        if(is_alpha)
+        QMessageBox::warning(this, "Connection", "Enter Port Number");
+        return;
+    }
+    else
+    {
+        for(l=0; l<Srv_Port_Num_str.size(); l++)
+        {
+            bool is_alpha = Srv_Port_Num_str[l].isLetter();
+            if(is_alpha)
+            {
+                QMessageBox::warning(this, "Connection", "Invalid Port Number");
+                return;
+            }
+        }
+
+        // Port Number check - Range(0 to 65535)
+        Srv_Port_Num = Srv_Port_Num_str.toInt();
+        if(((Srv_Port_Num > 65535) || (Srv_Port_Num < 0)))
         {
             QMessageBox::warning(this, "Connection", "Invalid Port Number");
             return;
         }
     }
 
-    // check is the port number in range
-    Srv_Port_Num = Srv_Port_Num_str.toInt();
-    if(!((Srv_Port_Num <= 65535) || (Srv_Port_Num >= 0)))
-    {
-        QMessageBox::warning(this, "Connection", "Invalid Port Number");
-        return;
-    }
+    //Initiate TCP connection
 
-    // After all the check, initiate connection
-
-    //qInfo() << "IP_Addr[0]:" << Srv_IP_Addr_strlist[0];
     clientsocket->connectToHost(Srv_IP_Addr_str, Srv_Port_Num);
-    if(clientsocket->waitForConnected(60000))
+    if(clientsocket->waitForConnected(60000))       //Timeout - 1Minute
     {
         QMessageBox::information(this, "Connection Status", "Connection Successful!!");
+
+        // Enable disconnect button
+        ui->pushButtonDisconnect->setEnabled(true);
 
         // Disable connection configuration field
         ui->pushButtonConnect->setDisabled(true);
@@ -119,9 +139,15 @@ void MainWindow::on_pushButtonConnect_clicked()
 
         //Enable channel configuration fields
         ui->groupBoxChannel->setEnabled(true);
+
+        //Enable log message field
+        ui->plainTextEditLogs->setEnabled(true);
     }
     else
     {
+        // on connection fail - capture error type and notify user
+        QAbstractSocket::SocketError err = QAbstractSocket::SocketError::UnknownSocketError;
+        err = clientsocket->error();
         QMessageBox::critical(this, "Connection Status", "Connection Failed!!");
     }
 }
@@ -139,6 +165,10 @@ void MainWindow::on_pushButtonDisconnect_clicked()
 
         // Disable channel and Log fields
         ui->groupBoxChannel->setDisabled(true);
+        ui->plainTextEditLogs->setDisabled(true);
+
+        // Disable disconnect button
+        ui->pushButtonDisconnect->setDisabled(true);
 
         // Enable connection configuration field
         ui->pushButtonConnect->setEnabled(true);
@@ -150,14 +180,26 @@ void MainWindow::on_pushButtonDisconnect_clicked()
 
 void MainWindow::on_pushButtonCapture_clicked()
 {
-    char log_config[3];
-    log_config[0] = (char)ui->checkBoxAnalog->isChecked();
-    log_config[1] = (char)ui->checkBoxDigital->isChecked();
-    log_config[2] = ui->comboBoxSampleRate->currentIndex();
+    char log_config[4];
+    // Offset 1 is added to avoid null bytes in TCP payload
+    log_config[0] = (uint8_t)ui->checkBoxAnalog->isChecked() + PAYLOAD_OFFSET;
+    log_config[1] = (uint8_t)ui->checkBoxDigital->isChecked() + PAYLOAD_OFFSET;
+    log_config[2] = (uint8_t)ui->comboBoxSampleRate->currentIndex() + PAYLOAD_OFFSET;
+    log_config[3] = LOG_OPERATION_START;
 
-    qInfo() << "analog" << log_config[0];
-    qInfo() << "samplerate:" << log_config[2];
-    qInfo() << "digital" << log_config[1];
+    if(!((uint8_t)ui->checkBoxDigital->isChecked() || (uint8_t)ui->checkBoxAnalog->isChecked()))
+    {
+        QMessageBox::critical(this, "Configuration", "Select channel!!");
+        return;
+    }
+
+    ui->plainTextEditLogs->clear();
+
+#if ENABLE_DEBUG
+    qInfo() << "analog" << log_config[1];
+    qInfo() << "digital" << log_config[2];
+    qInfo() << "samplerate:" << log_config[3];
+#endif
 
     // Disable channel configuration fields
     ui->checkBoxAnalog->setDisabled(true);
@@ -173,6 +215,16 @@ void MainWindow::on_pushButtonCapture_clicked()
 
 void MainWindow::on_pushButtonStop_clicked()
 {
+    char log_config[4];
+    // Offset 1 is added to avoid null bytes in TCP payload
+    log_config[0] = (uint8_t)ui->checkBoxAnalog->isChecked() + PAYLOAD_OFFSET;
+    log_config[1] = (uint8_t)ui->checkBoxDigital->isChecked() + PAYLOAD_OFFSET;
+    log_config[2] = (uint8_t)ui->comboBoxSampleRate->currentIndex() + PAYLOAD_OFFSET;
+    log_config[3] = LOG_OPERATION_STOP;
+
+    clientsocket->write(log_config);
+    clientsocket->waitForBytesWritten(1000);
+
     // Enable channel configuration fields
     ui->checkBoxAnalog->setEnabled(true);
     ui->checkBoxDigital->setEnabled(true);
--- mainwindow.h
+++ mainwindow.h
@@ -10,9 +10,18 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+#define LOG_OPERATION_START 2
+#define LOG_OPERATION_STOP  1
+#define PAYLOAD_OFFSET      1
+#define ENABLE_DEBUG        0
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
+private:
+    Ui::MainWindow *ui;
+    QTcpSocket *clientsocket;
 
 public:
     MainWindow(QWidget *parent = nullptr);
@@ -20,17 +29,9 @@ public:
 
 private slots:
     void on_pushButtonConnect_clicked();
-
     void on_pushButtonDisconnect_clicked();
-
     void on_pushButtonCapture_clicked();
-
     void on_pushButtonStop_clicked();
-
     void readyRead();
-
-private:
-    Ui::MainWindow *ui;
-    QTcpSocket *clientsocket;
 };
 #endif // MAINWINDOW_H
